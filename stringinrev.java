fc5296ad1ba958a6c6fb37d0f9c9d4c23deb67c5
ShreshthiTayal <101048165+ShreshthiTayal@users.noreply.github.com>
2024-07-01 15:03:53 +0530

    Add files via upload

---
+++ stringinrev.java
@@ -0,0 +1,16 @@
+public class stringinrev{
+    public static void printRev(String str, int index){
+        if(index==0)
+        {
+            System.out.println(str.charAt(index));
+            return;
+        }
+        System.out.println(str.charAt(index));
+        printRev(str, index-1);
+    }
+    public static void main(String[] args)
+    {
+        String str="abcde";
+        printRev(str, str.length()-1);
+    }
+}
